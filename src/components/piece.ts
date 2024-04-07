be2a3df1fbb14b82d657433ea4348a55b8b33934
rdcsq <git@rdcsq.me>
2024-04-06 23:08:17 -0700

    draw pieces

---
+++ src/components/piece.ts
@@ -0,0 +1,120 @@
+import p5 from "p5";
+
+type PieceRotation = "horizontal" | "vertical";
+
+export function drawPiece(
+  s: p5,
+  firstHalf: number,
+  secondHalf: number,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+) {
+  if (firstHalf < 0 || firstHalf > 6 || secondHalf < 0 || secondHalf > 6)
+    throw Error("number of dots is out of range");
+
+  const pieceRotation: PieceRotation =
+    width > height ? "horizontal" : "vertical";
+
+  s.push();
+  s.fill(255, 255, 255);
+  s.stroke(0, 0, 0);
+
+  s.rect(x, y, width, height);
+
+  let lineXStart: number;
+  let lineXEnd: number;
+  let lineYStart: number;
+  let lineYEnd: number;
+
+  if (pieceRotation == "horizontal") {
+    lineXStart = x + width / 2;
+    lineXEnd = lineXStart;
+    lineYStart = y;
+    lineYEnd = y + height;
+  } else {
+    lineXStart = x;
+    lineXEnd = x + width;
+    lineYStart = y + height / 2;
+    lineYEnd = lineYStart;
+  }
+
+  s.line(lineXStart, lineYStart, lineXEnd, lineYEnd);
+
+  s.fill(0, 0, 0);
+
+  drawDotGrid(
+    s,
+    firstHalf,
+    x,
+    y,
+    pieceRotation == "horizontal" ? width / 2 : width,
+    pieceRotation,
+  );
+
+  drawDotGrid(
+    s,
+    secondHalf,
+    pieceRotation == "horizontal" ? x + width / 2 : x,
+    pieceRotation == "horizontal" ? y : y + height / 2,
+    pieceRotation == "horizontal" ? width / 2 : width,
+    pieceRotation,
+  );
+
+  s.pop();
+}
+
+function drawDotGrid(
+  s: p5,
+  dots: number,
+  x: number,
+  y: number,
+  size: number, // size of square in which its contained
+  pieceRotation: PieceRotation,
+) {
+  const spacing = size / 10;
+  const d = size * 0.2;
+
+  if (dots < 6) {
+    // middle dot
+    if (dots == 5 || dots == 1 || dots == 3) {
+      s.circle(x + size / 2, y + size / 2, d);
+    }
+
+    if (dots == 1) return;
+
+    x += d / 2;
+    y += d / 2;
+
+    // dot top left
+    s.circle(x + spacing, y + spacing, d);
+    // dot bottom right
+    s.circle(x + size - spacing - d, y + size - spacing - d, d);
+
+    if (dots >= 4) {
+      // dot top right
+      s.circle(x + size - spacing - d, y + spacing, d);
+      // dot bottom left
+      s.circle(x + spacing, y + size - spacing - d, d);
+    }
+  } else {
+    if (pieceRotation == "horizontal") {
+      x += d / 2 + spacing;
+      y += size * 0.2 + spacing;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 2; j++) {
+          s.circle(x + i * (d + spacing), y + j * (d + spacing * 2), d);
+        }
+      }
+    } else {
+      y += d / 2 + spacing;
+      x += size * 0.2 + spacing;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 2; j++) {
+          s.circle(x + j * (d + spacing * 2), y + i * (d + spacing), d);
+        }
+      }
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import p5 from "p5";
+import { drawPiece } from "./components/piece";
 
 // @ts-ignore
 window.p5 = p5;
@@ -13,4 +14,11 @@ s.draw = () => {
   s.background(255, 255, 255);
   s.fill(0, 0, 0);
   s.text(`${s.width}x${s.height}`, 0, 10, 100);
+
+  for (let i = 1; i <= 6; i++) {
+    for (let j = 1; j <= 6; j++) {
+      drawPiece(s, i, j, 40 + 100 * i + 10 * i, 40 + 50 * j + 10 * j, 100, 50);
+      drawPiece(s, i, j, 960 + 50 * i + 10 * i, 40 + 100 * j + 10 * j, 50, 100);
+    }
+  }
 };
